ab7dbd37676b20c7aabaf659afbce0bc61d8c1b1
Trevor Wheeler <sendtrevmail@gmail.com>
2019-03-06 18:29:12 +1000

    update and remove depreciated packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('webpack-cleanup-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: {
         app: './src/index.js',
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: __dirname + '/dist',
     },
     // devtool: 'inline-source-map',
     devServer: {
@@ -20,47 +20,67 @@ module.exports = {
     },
     module: {
         rules: [
+            // {
+            //     test: /\.css$/,
+            //     use: ExtractTextPlugin.extract({
+            //         fallback: 'style-loader',
+            //         use: ['css-loader', 'postcss-loader']
+            //     })
+            // },
             {
                 test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'postcss-loader']
-                })
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    'postcss-loader'
+                ]
             },
             {
                 test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {loader: 'css-loader', options: {sourceMap: true}},
-                        {loader: 'postcss-loader', options: {sourceMap: true}},
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {loader: 'css-loader', options: {sourceMap: true}},
+               {loader: 'postcss-loader', options: {sourceMap: true}},
                         {loader: 'sass-loader', options: {sourceMap: true}}
-                        ]
-                })
+                ]
             },
+            // {
+            //     test: /\.scss$/,
+            //     use: ExtractTextPlugin.extract({
+            //         fallback: 'style-loader',
+            //         use: [
+            //             {loader: 'css-loader', options: {sourceMap: true}},
+            //             {loader: 'postcss-loader', options: {sourceMap: true}},
+            //             {loader: 'sass-loader', options: {sourceMap: true}}
+            //             ]
+            //     })
+            // },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
-                }
+            }
         ]
     },
 
     plugins: [
-      
+
         new CleanWebpackPlugin(['dist']),
-        new ExtractTextPlugin('styles.css'),
+        // new ExtractTextPlugin('styles.css'),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'src/views/index.html',
-            chunks: ['app']
+
         }),
         new BrowserSyncPlugin({
             host: 'localhost',
-            port: 3000,
+            port: 8085,
             proxy: 'http://localhost:8080/'
-        },
-        {
+        }, {
             reload: false
         }),
         new webpack.ProvidePlugin({
@@ -72,10 +92,11 @@ module.exports = {
             // Util: "exports-loader?Util!bootstrap/js/dist/util",
             // Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
             // ...
-          }),
-          new CopyWebpackPlugin([
-            { from: 'src/img', to: 'img' }
-          ]),
-     
+        }),
+        new CopyWebpackPlugin([{
+            from: 'src/img',
+            to: 'img'
+        }]),
+
     ]
 };
